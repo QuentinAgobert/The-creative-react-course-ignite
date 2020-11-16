2290fb9dc3545f956da3c7cb03cbda0fefe7b56e
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2020-11-16 14:30:46 +0100

    5. Fetching Our First Data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { loadGames } from './actions/gamesAction';
+
 
 function App() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadGames());
+    });
   return (
     <div className="App">
         <h1>Hello Ignite</h1>
