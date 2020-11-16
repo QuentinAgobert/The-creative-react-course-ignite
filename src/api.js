084636d165f606e0c9b9570f6beda8a4753e016f
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2020-11-16 15:45:33 +0100

    8. Styling Up The Cards

--- src/api.js
+++ src/api.js
@@ -29,9 +29,11 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Games
-const popular_games = `games?date=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?date=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?date=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
+console.log(`${base_url}${upcoming_games}`);
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
