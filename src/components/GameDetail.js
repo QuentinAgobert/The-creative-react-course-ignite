5a936470b9082b3b55c1314a7b3c15df4bf16096
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2020-11-19 12:20:28 +0100

    19. Rendering Stars

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // Styling & Animation
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, startVisualElementAnimation } from 'framer-motion';
 // Redux
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -13,6 +13,9 @@ import xbox from '../img/xbox.svg';
 import nintendo from '../img/nintendo.svg';
 import apple from '../img/apple.svg';
 import gamepad from '../img/gamepad.svg';
+// Star Images
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = ({ pathId }) => {
 
@@ -44,6 +47,20 @@ const GameDetail = ({ pathId }) => {
         }
     };
 
+    // Get Stars
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push( <img alt="star" key={i} src={starFull}></img> );
+            } else {
+                stars.push( <img alt="star" key={i} src={starEmpty}></img> );
+            }
+        }
+        return stars;
+    };
+
     // Data
     const { screen, game, isLoading } = useSelector((state) => state.detail);
     return (
@@ -55,6 +72,7 @@ const GameDetail = ({ pathId }) => {
                             <div className="rating">
                                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                                 <p>Rating: {game.rating}</p>
+                                {getStars()}
                             </div>
                             <Info>
                                 <h3>Platforms</h3>
@@ -121,6 +139,11 @@ const States = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    img {
+        width: 2rem;
+        height: 2rem;
+        display: inline;
+    }
 `;
 
 const Info = styled(motion.div)`
